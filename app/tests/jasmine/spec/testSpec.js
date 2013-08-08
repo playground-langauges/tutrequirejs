018d1b88a5142977630e3fad2a3a17790e2d031a
buzzshiva <buzz.shiva@gmail.com>
2013-08-08 22:27:56 +0530

    added new spec/testSpec.js

--- app/tests/jasmine/spec/testSpec.js
+++ app/tests/jasmine/spec/testSpec.js
@@ -1,5 +1,5 @@
 // Test Subtract and Square functionality
-define(['subtract', 'square'], function (sub, sq) {
+define(['subtract', 'square','multiply'], function (sub, sq, mul) {
 	var that = this, a = 13, b = 121;
 	describe("Subtract Square Functionality Testcase:", function() {
 		beforeEach(function() {
@@ -18,6 +18,15 @@ define(['subtract', 'square'], function (sub, sq) {
 		it("Is Subtraction of 18 and 1 equals to 13", function() {
 			expect(a).toBe(sub(18,1));
 		});
+
+
+    it("Is multiplication of 13 and 1 equals to 13", function() {
+      expect(a).toBe(mul(13,1));
+    });
+
+    it("Is multiplication of 14 and 1 equals to 13", function() {
+      expect(a).toBe(mul(14,1));
+    });
 
 		it("Is square of 11 equals 121", function() {
 			expect(b).toBe(sq(11));
